5ce2a1cc48a795c74be92a58a1c6bc9d5593fdf7
Aina Lopez <aina.lopez@barcelonagse.eu>
2016-02-10 10:52:59 +0100

    Update package description

--- Package- Milestone I/ABBABAkaggle/ABBABAkaggle/R/bestABBABA.R
+++ Package- Milestone I/ABBABAkaggle/ABBABAkaggle/R/bestABBABA.R
@@ -1,15 +1,41 @@
+# ----------------------------------------------------------------------
+# Random Forest Classifier
+# ----------------------------------------------------------------------
+#' Random Forest classifier 
+#' 
+#' multi-class classifier that predicts the labels for the test data set using a random forest. 
+#'
+#' @param data BLABLABLA.
+#' @param test BLABLABLA.
+#' @param file Logical, set to TRUE if you want to create a csv file with the Id's and corresponding predicted classes, by deafult set to FALSE.
+#' @return A dataframe with following elements: Id, predicted class.
+#' @import assertthat 
+#' @import tm 
+#' @import randomForest 
+#' @export
+#' @examples
+#' # Given a training and a test set
+#' bestABBABA(training, test)
+
+
 
 
 bestABBABA <- function(data, test, file = FALSE){
 
-  # Load libraries
+  # Load libraries (NECESSARY?)
+  library(assertthat)
   library(tm)
   library(randomForest)
+  
+  # Check the Input
+  not_empty(data)
+  not_empty(test)
+  assert_that(is.logical(file))
+  
+  
   set.seed(111)
 
-
   # TRAIN DATA: extract features from the URL characters in the training data
-
   # year, month, day
   data$Year         <- substr(data$url,21,24)
   data$Month        <- substr(data$url,26,27)
@@ -29,7 +55,6 @@ bestABBABA <- function(data, test, file = FALSE){
   text.input <- get.title(data)
 
   # text variables
-
   text.variables<-function(text.input, minimum){
     docs <- Corpus(VectorSource(text.input))
     dtm  <- DocumentTermMatrix(docs, control=list(bounds = list(global = c(minimum,Inf))))
@@ -39,7 +64,7 @@ bestABBABA <- function(data, test, file = FALSE){
 
   textvars <- text.variables(text.input,150)
   
-  #remove problematic words (error in random forest)
+  #remove problematic words (errors in random forest)
   textvars <- textvars[,-which(colnames(textvars)=='2013')]
   textvars <- textvars[,-which(colnames(textvars)=='2014')]
   textvars <- textvars[,-which(colnames(textvars)=='for')]
@@ -47,7 +72,6 @@ bestABBABA <- function(data, test, file = FALSE){
   words <- colnames(textvars)
   
   #remove 2's from the matrix to have binary variables
-  
   for( i in 1:dim(textvars)[2]){
     textvars[,i] <- ifelse(textvars[,i]>1, 1, textvars[,i])
     }
@@ -55,8 +79,8 @@ bestABBABA <- function(data, test, file = FALSE){
   textvars <- apply(textvars, 2, as.factor)
   train    <- cbind(data[, -2], textvars)
 
-  # TEST DATA: extract features from the URL characters in the test data
   
+  # TEST DATA: extract features from the URL characters in the test data
   # year, month, day
   test$Year         <- substr(test$url,21,24)
   test$Month        <- substr(test$url,26,27)
@@ -69,7 +93,6 @@ bestABBABA <- function(data, test, file = FALSE){
   test.textvars <- inspect(dtm[,words])
   
   #remove 2's amd 3's
-  
   for( i in 1:dim(test.textvars)[2]){
     test.textvars[,i] <- ifelse(test.textvars[,i]>1, 1, test.textvars[,i])
     }
@@ -78,9 +101,10 @@ bestABBABA <- function(data, test, file = FALSE){
   testing <- cbind(test[,-2],test.textvars)
 
 
-  # Keep only the most important features found using the function RRF (package "RRF")
+  # Keep only the most important features. Feature indexes found using the function RRF (package "RRF").
   training.rv<- train[c(1,2,3,4,5,7,8,9,10,11,12,13,14,15,17,18,21,22,23,25,26,27,28,29,30,31,32,33,34,35,36,38,39,40,41,42,43,44,45,46,47,48,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,76)]
   
+  # Train the random forest
   forest <- randomForest(factor(popularity) ~ . , 
                        data=training.rv, 
                        replace = FALSE,
@@ -90,6 +114,7 @@ bestABBABA <- function(data, test, file = FALSE){
   prediction <- predict(forest, testing)
   solution   <- data.frame(Id = test$id, popularity = as.numeric(prediction2))
   
+  # Write csv
   if(file){
     write.csv(solution, file = "predictions.csv", row.names = FALSE)
     }
